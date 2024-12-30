c6ba89bb28fb84d0d68e377343349d435f7d4172
C <C@cmail.c>
2024-12-30 11:32:52 +0100

    sending files to backent successfully on 90%

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ submitButton.setAttribute(`id`, `submitButton`);
 fileInput.setAttribute(`id`, `file-input`);
 fileInput.setAttribute(`type`, `file`);
 fileInput.setAttribute(`name`, `file-input`);
+fileInput.setAttribute(`multiple`, true);
 
 
 title.textContent = `Uploading`;
@@ -44,13 +45,42 @@ fileField.addEventListener('click', () => {
                 selectedFilesArray.forEach(file => {
                     fileNames.push(file.name)
                 });
-                const filesString = fileNames.join(`,`);
+                const filesString = fileNames.join(`, `);
                 fileField.textContent = filesString;
                 console.log(filesString);
             } else
                 fileField.textContent = `Aucun fichier sélectionné.`; 
         });
     } catch(err) {
-        console.log(`Error on filField clicked : ${err}`);
+        console.error(`Error: ${err.message}`);
+    }
+});
+
+submitButton.addEventListener(`click`, async () => {
+    try {
+        const files = fileInput.files;
+        if (files.length === 0) {
+            alert(`Veuillez sélectionner le fichier`);
+            return; 
+        }
+        
+        const fromData = new FormData();
+        const filesArray = Array.from(files);
+        filesArray.forEach(file => {
+            fromData.append(`files`, file);
+        });
+        const response = await fetch(`http://127.0.0.1:20000`, {
+            method: `POST`,
+            body: fromData
+        });
+
+        if(response.ok) {
+            const result = await response.json();
+            console.log(`Fichier envoyé avec succès: ${result}`);
+        } else
+            alert(`Erreur lors de l'envoi du fichier`);
+        
+    } catch (err) {
+        console.error(`Erreur lors de la connexion au serveur >> ${err.message}`);
     }
 });
