0e18bdba72a28322e3cc2f88280e7abf5e97376b
C <C@cmail.c>
2025-01-09 10:45:12 +0100

    R.A.S

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const childrenAppender = (parentElemet,childElements) => {
+const childrenAppender = (parentElemet, childElements) => {
     childElements.forEach(childElement => {
         parentElemet.appendChild(childElement);
     });
@@ -26,10 +26,12 @@ fileField.classList.add(`fileField`);
 submitButton.setAttribute(`id`, `submitButton`);
 downloadButton.classList.add(`downloadButton`);
 
-fileInput.setAttribute(`id`, `file-input`);
-fileInput.setAttribute(`type`, `file`);
-fileInput.setAttribute(`name`, `file-input`);
-fileInput.setAttribute(`multiple`, true);
+setAttributes(fileInput, {
+    id: `file-input`,
+    type: `file`,
+    name: `file-input`,
+    multiple: true
+});
 
 
 title.textContent = `Uploading`;
@@ -62,44 +64,7 @@ fileField.addEventListener(`click`, () => {
                 fileField.textContent = `Aucun fichier sélectionné.`; 
         });
     } catch(err) {
-        console.error(`Error: ${err.message}`);
-    }
-});
-
-submitButton.addEventListener(`click`, async () => {
-    try {
-        const files = fileInput.files;
-        if (files.length === 0) {
-            alert(`Veuillez sélectionner le fichier`);
-            return; 
-        }
-        
-        const formData = new FormData();
-        const filesArray = Array.from(files);
-        filesArray.forEach(file => {
-            formData.append(`files`, file);
-        });
-        const response = await fetch(`http://127.0.0.1:20000/upload`, {
-            method: `POST`,
-            body: formData
-        });
-
-        const result = await response.json();
-        const fileNames = result.files.join(', ');
-        const message = result.message;
-        const errorServer = result.error;
-
-        if(response.ok) {
-            console.log(`${message}: ${fileNames}`);
-            alert(`${message}: ${fileNames}`);
-        } else {
-            console.log(`${message} >> ${errorServer}`);
-            alert(`${message} >> ${errorServer}`);
-        }
-        
-    } catch (err) {
-        console.error(`Erreur lors de la connexion au serveur >> ${err}`);
-        alert(`Erreur lors de la connexion au serveur >> ${err}`);
+        console.log(`Error on filField clicked : ${err}`);
     }
 });
 
@@ -137,6 +102,8 @@ submitButton.addEventListener(`click`, async () => {
 
 downloadButton2.addEventListener(`click`, () => {
     window.location.href = `http://127.0.0.1:20000/download/importFileD`;
+    board.removeChild(downloadButton);
+    board.appendChild(submitButton);
 });
 
 downloadButton.addEventListener(`click`, () => {
